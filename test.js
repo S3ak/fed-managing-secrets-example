f835c63d2424b551242b774f95878c83a5985ddd
Monde Sineke <nsineke@gmail.com>
2023-10-24 09:49:40 +0200

    chore: :white_check_mark: Use env var

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 import "dotenv/config";
 
-console.log("Hello World");
+console.log(process.env.API_USER);
+
+function test(expectedSecret) {
+  const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+  console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  return secretMatches;
+}
+
+test(42);
